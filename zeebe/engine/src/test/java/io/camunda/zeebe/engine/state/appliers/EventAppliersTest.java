48b068530028a0356aeade471e45f7f28fef3774
Oleksii Ivanov <oleksii.ivanov-ext@camunda.com>
2024-10-08 14:01:18 +0300

    feat: fix shouldRegisterApplierForAllIntents test

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/appliers/EventAppliersTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/appliers/EventAppliersTest.java
@@ -20,6 +20,7 @@ import io.camunda.zeebe.engine.state.mutable.MutableProcessingState;
 import io.camunda.zeebe.protocol.record.RecordValue;
 import io.camunda.zeebe.protocol.record.intent.Intent;
 import io.camunda.zeebe.protocol.record.intent.ProcessIntent;
+import io.camunda.zeebe.protocol.record.intent.TenantIntent;
 import io.camunda.zeebe.protocol.record.intent.management.CheckpointIntent;
 import java.util.Arrays;
 import org.junit.jupiter.api.Assertions;
@@ -167,7 +168,9 @@ public class EventAppliersTest {
             .flatMap(c -> Arrays.stream(c.getEnumConstants()))
             .filter(Intent::isEvent)
             // CheckpointIntent is not handled by the engine
-            .filter(intent -> !(intent instanceof CheckpointIntent));
+            .filter(intent -> !(intent instanceof CheckpointIntent))
+            // todo delete this filter after https://github.com/camunda/camunda/issues/22841
+            .filter(intent -> !(intent instanceof TenantIntent));
 
     // when
     eventAppliers.registerEventAppliers(mock(MutableProcessingState.class));
